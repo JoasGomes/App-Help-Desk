8d94731b214b4acfd077debb9abeac16960f3c30
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-30 19:19:57 -0300

    appHelpDesk-part6

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -3,12 +3,18 @@
     session_start();
     //variavel que verifica se a autenticação foi feita
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = array(1 => 'Administrador', 2 => 'Usuário');
 
 
     //usuarios do sistema
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br','senha' => '123456'),
-        array('email' => 'user@teste.com.br','senha' => 'abcd'),
+        array('id' => 1, 'email' => 'adm@teste.com.br','senha' => '1234', 'perfil_id' => 1),
+        array('id' => 2, 'email' => 'user@teste.com.br','senha' => '1234', 'perfil_id' => 1),
+        array('id' => 3, 'email' => 'jose@teste.com.br','senha' => '1234', 'perfil_id' => 2),
+        array('id' => 4, 'email' => 'maria@teste.com.br','senha' => '1234', 'perfil_id' => 2),
 
     );
 
@@ -18,6 +24,8 @@
 
         if($user['email'] == $_POST['email'] &&  $user['senha'] ==  $_POST['senha']){
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
         }
         
     }
@@ -25,8 +33,8 @@
     if($usuario_autenticado){
         echo 'usuario autenticado';
         $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['x'] = 'um valor';
-        $_SESSION['y'] = 'outro valor';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
         header('Location: home.php');
     } else{
         $_SESSION['autenticado'] = 'NÃO';
