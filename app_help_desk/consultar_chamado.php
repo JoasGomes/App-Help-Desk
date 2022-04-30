82b291ff1b32cdc092425a8bacd9cf14db9ca7ee
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-30 17:31:00 -0300

    appHelpDesk-part5

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -4,6 +4,26 @@
 
 ?>
 
+<?php
+
+  //chamados
+  $chamados = array();
+
+  //abrir o arquivo
+  $arquivo = fopen('arquivo.hd', 'r');
+
+  //enquanto houver registros a serem recuperados
+  while(!feof($arquivo)){//testa pelo fim do arquivo
+    //linhas
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  //fechar o arquivo aberto
+  fclose($arquivo);
+
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -44,24 +64,28 @@
             </div>
             
             <div class="card-body">
+
+              <? foreach($chamados as $chamado) { ?>
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                <?php
+                  
+                  $chamado_dados = explode('#',$chamado);
 
+                  if(count($chamado_dados) < 3){
+                    continue;
+                  }
+                  
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                    <p class="card-text"><?= $chamado_dados[2] ?></p>
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
